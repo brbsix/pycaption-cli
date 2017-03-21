64a2940a66db74d1f7b36cb47bb5b2f0a53f8299
Six <brbsix@gmail.com>
2017-03-20 22:19:01 -0700

    Modernize setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,31 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Application setup script"""
+
 import os
 from setuptools import setup, find_packages
 
-README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                           'README.md')
 
-dependencies = [
-    'pycaption'
-]
+def read(path):
+    """Return file contents."""
+    with open(path) as file_object:
+        return file_object.read()
+
+
+# allow setup.py to be run from any path
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 setup(
     name='pycaption-cli',
     version='0.1',
     description='Command line caption conversion',
+    long_description=read('README.md'),
     author='Joe Norton',
     author_email='joey@nortoncrew.com',
     url='https://github.com/jnorton001/pycaption-cli',
-    install_requires=dependencies,
+    install_requires=['pycaption'],
     packages=find_packages(),
     include_package_data=True,
-    entry_points=dict(
-        console_scripts=['pycaption=pycapcli.caption_converter:main']),
+    entry_points={
+        'console_scripts': ['pycaption=pycapcli.caption_converter:main'],
+    }
 )
